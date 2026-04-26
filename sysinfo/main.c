bb66b453e2b9b44e1a89ecae1379dd422a54e216
bimcsr <bmcsr@proton.me>
2026-04-26 11:51:07 +0200

    fixed CPU display

--- sysinfo/main.c
+++ sysinfo/main.c
@@ -1,62 +1,50 @@
 #include <stdio.h>
 #include <string.h>
 
+void uptime() {
 
+  FILE *f = fopen("/proc/uptime", "r");
+  double uptime;
+  fscanf(f, "%lf", &uptime);
+  fclose(f);
 
-void uptime(){
+  // test
+  int minutes = uptime / 60;
+  int hours = minutes / 60;
 
-    FILE *f = fopen("/proc/uptime", "r");
-    double uptime;
-    fscanf(f, "%lf", &uptime);
-    fclose(f);
-
-    //test
-    int minutes = uptime/60;
-    int hours = minutes/60;
-
-    printf("System has been up for %d hours (or %d minutes (or %.0f seconds))\n", hours, minutes, uptime);
-
+  printf("System has been up for %d hours (or %d minutes (or %.0f seconds))\n",
+         hours, minutes, uptime);
 }
 
-void cpumodel(){
+void cpumodel() {
 
-    FILE *f = fopen("/proc/cpuinfo", "r");
-    char line[256];
+  FILE *f = fopen("/proc/cpuinfo", "r");
+  char line[256];
 
-    while (fgets(line, sizeof(line), f)){
-        if (strncmp(line, "model name", 10) == 0){
-            char *value = strchr(line, ':');
-            value += 2;   
-            printf("CPU manufacturer: ");
-            while (*value != ' '){
-                printf("%c", *value);
-                value++;
-            }
-            printf("\n");
-            value++;
-            printf("CPU model: %s", value);
-            break;
-        }
+  while (fgets(line, sizeof(line), f)) {
+    if (strncmp(line, "model name", strlen("model name")) == 0) {
+
+      char *value = strchr(line, ':');
+      value++;
+      while (*value == ' ' || *value == '\t') {
+        value++;
+      }
+      printf("CPU manufacturer: ");
+      while (*value != ' ') {
+        printf("%c", *value);
+        value++;
+      }
+      printf("\n");
+      value++;
+      printf("CPU model: %s", value);
+      break;
     }
-    fclose(f);
+  }
+  fclose(f);
 }
 
 int main() {
 
-    uptime();
-    cpumodel();
-
+  //  uptime();
+  cpumodel();
 }
-
-
-
-
-
-/**
-  while (fgets(line, sizeof(line), f)) {
-  if(strncmp(line, "model name", 10) == 0) {
-  printf("CPU %s", line);
-  break;
-  }
-  }
- **/   
