5e94672930a25dd02d1f748e3c46318b2dbc7338
bimcsr <bmcsr@proton.me>
2026-04-26 10:38:22 +0200

    first commit

---
+++ sysinfo/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+void uptime(){
+
+    FILE *f = fopen("/proc/uptime", "r");
+    double uptime;
+    fscanf(f, "%lf", &uptime);
+    fclose(f);
+
+    int minutes = uptime/60;
+    int hours = minutes/60;
+
+    printf("System has been up for %d hours (or %d minutes (or %.0f seconds))\n", hours, minutes, uptime);
+
+}
+
+void cpumodel(){
+
+    FILE *f = fopen("/proc/cpuinfo", "r");
+    char line[256];
+
+    while (fgets(line, sizeof(line), f)){
+        if (strncmp(line, "model name", 10) == 0){
+            char *value = strchr(line, ':');
+            value += 2;   
+            printf("CPU manufacturer: ");
+            while (*value != ' '){
+                printf("%c", *value);
+                value++;
+            }
+            printf("\n");
+            value++;
+            printf("CPU model: %s", value);
+            break;
+        }
+    }
+    fclose(f);
+}
+
+int main() {
+
+    uptime();
+    cpumodel();
+
+}
+
+
+
+
+
+/**
+  while (fgets(line, sizeof(line), f)) {
+  if(strncmp(line, "model name", 10) == 0) {
+  printf("CPU %s", line);
+  break;
+  }
+  }
+ **/   
